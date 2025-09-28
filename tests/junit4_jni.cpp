29291c841b2b91ea391b283a194f2bc7f58630e4
e_gleba <i@egleba.ru>
2025-09-28 17:50:49 +0300

    junit refactor

--- tests/junit4_jni.cpp
+++ tests/junit4_jni.cpp
@@ -1,80 +1,109 @@
 #include "main.h"
 
-#include <android/log.h>
+/// See https://docs.oracle.com/javase/8/docs/technotes/guides/jni/spec/functions.html
 #include <jni.h>
+#include <android/log.h>
 
 #include <iostream>
 
 #include <doctest/doctest.h>
 
-class android_logbuf final : public std::streambuf {
-    static constexpr auto* TAG = "native_tests";
-    static constexpr size_t BUF_SIZE = 512;
-    char buf[BUF_SIZE];
+namespace {
+    struct android_logbuf final: std::streambuf {
+        static constexpr const auto tag = "native_tests";
+        static constexpr size_t buf_size = 512;
+        char buf_[buf_size];
 
-public:
-    android_logbuf() { setp(buf, buf + BUF_SIZE - 1); }
-    ~android_logbuf() { flush_buffer(); }
+        android_logbuf() { setp(buf_, buf_ + buf_size - 1); }
 
-protected:
-    int_type overflow(int_type ch) override {
-        if (ch == traits_type::eof()) return traits_type::not_eof(ch);
-        *pptr() = char(ch);
-        pbump(1);
-        if (ch == '\n' || pptr() == epptr()) flush_buffer();
-        return ch;
-    }
-    int sync() override {
-        flush_buffer();
-        return 0;
-    }
+        ~android_logbuf() override { overflow(traits_type::eof()); }
 
-private:
-    void flush_buffer() {
-        auto len = pptr() - pbase();
-        if (len <= 0) return;
-        // strip trailing newline so it’s not doubled
-        size_t real_len = (buf[len - 1] == '\n') ? len - 1 : len;
-        __android_log_write(ANDROID_LOG_INFO, TAG,
-                            std::string(buf, real_len).c_str());
-        setp(buf, buf + BUF_SIZE - 1);
-    }
-};
+        int_type overflow(int_type ch) override {
+            if (ch == traits_type::eof() || pptr() == epptr()) {
+                flush();
+                if (ch == traits_type::eof()) return traits_type::not_eof(ch);
+            }
+            *pptr() = static_cast<char>(ch);
+            pbump(1);
+            if (ch == '\n') flush();
+            return ch;
+        }
 
-static android_logbuf g_logbuf;
+        int sync() override { flush(); return 0; }
 
-static bool const redirect_cout = ([] {
-    std::cout.rdbuf(&g_logbuf);
-    return true;
-})();
+    private:
+        void flush() {
+            if (auto n = pptr() - pbase(); n > 0) {
+                // Remove trailing \n to avoid double newline in logcat
+                if (pbase()[n - 1] == '\n') --n;
+                if (n > 0) {
+                    std::array<char, buf_size> temp;
+                    std::copy(pbase(), pbase() + n, temp.data());
+                    temp[n] = '\0';
+                    __android_log_write(ANDROID_LOG_INFO,
+                                        tag, temp.data());
+                }
+                setp(buf_, buf_ + buf_size - 1);
+            }
+        }
+    };
+
+    static const bool android_log_redirected = [] {
+        static android_logbuf buf;
+        std::cout.rdbuf(&buf);
+        std::cerr.rdbuf(&buf);
+        return true;
+    }();
+}
 
 extern "C" JNIEXPORT jobjectArray JNICALL
-Java_com_egleba_app_AppActivityTest_getTestNames(JNIEnv* env, jclass) {
-    jclass strCls = env->FindClass("java/lang/String");
+Java_com_egleba_app_AppActivityTest_getTestNames(JNIEnv *env, const jclass) {
+    constexpr const char *const jstring_class_name = "java/lang/String";
+    const jclass java_string_class = env->FindClass(jstring_class_name);
+    if (!java_string_class) {
+        env->ExceptionDescribe();
+
+        using namespace std::string_literals;
+        throw std::runtime_error(
+                "java class not found: "s + jstring_class_name);
+    }
 
     const std::set<std::string> tests = get_all_tests();
-    jobjectArray arr = env->NewObjectArray(
-        static_cast<int>(tests.size()), strCls, nullptr);
+    const jobjectArray tests_to_run = env->NewObjectArray(
+            static_cast<int>(tests.size()), java_string_class, nullptr);
+    if (!tests_to_run) {
+        throw std::runtime_error("failed to allocate java object array of length " +
+                                 std::to_string(tests.size()));
+    }
 
-    int i = 0;
-    for (auto& tc : tests) {
-        env->SetObjectArrayElement(arr, i, env->NewStringUTF(tc.data()));
+    jsize i = 0;
+    for (auto &tc: tests) {
+        env->SetObjectArrayElement(tests_to_run, i,
+                                   env->NewStringUTF(tc.data()));
+        env->ExceptionDescribe();
         i++;
     }
-    return arr;
+    return tests_to_run;
 }
 
 extern "C" JNIEXPORT jboolean JNICALL
-Java_com_egleba_app_AppActivityTest_runTest(JNIEnv* env, jclass,
-                                            jstring jname) {
-    const char* name = env->GetStringUTFChars(jname, nullptr);
+Java_com_egleba_app_AppActivityTest_runTest(JNIEnv *env, const jclass,
+                                            const jstring jname) {
+    const char *name = env->GetStringUTFChars(jname, nullptr);
+    if (!name)
+    {
+        throw std::runtime_error("failed to convert jstring to char*");
+    }
 
-    doctest::Context ctx;
-    ctx.setOption("test-case", name);
-    ctx.setOption("no-exitcode", true); // todo: make a comment why critical
-    ctx.setOption("duration", true);
+    doctest::Context context;
+    context.setOption("test-case", name);
+    context.setOption("duration", true);
 
-    const int res = ctx.run();
+    // CRITICAL: Prevents doctest from calling exit() which would
+    // terminate the Android process instead of returning to Java
+    context.setOption("no-exitcode", true);
+
+    const int result = context.run();
     env->ReleaseStringUTFChars(jname, name);
-    return res == 0;
+    return result == 0 ? JNI_TRUE : JNI_FALSE;
 }
